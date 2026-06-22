6250208ba2a145a28814a02889749efd6f62b5f7
barath1248 <barathrv124@gmail.com>
2026-06-22 13:14:54 +0530

    Added functions code in app10

--- src/com/pack1/Jdbc_APP10.java
+++ src/com/pack1/Jdbc_APP10.java
@@ -1,6 +1,7 @@
 package com.pack1;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Types;
+//import java.sql.SQLException;
 import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.CallableStatement;
@@ -49,8 +50,51 @@ public class Jdbc_APP10 {
 			e.printStackTrace();
 		}
 	}
+	void meth2() {
+		Connection con=connect();
+		try {
+			CallableStatement cstmt=con.prepareCall("{call RetriveEmpData(?,?,?,?,?)}");
+			System.out.println("Enter Employee ID:");
+			String id=sc.nextLine();
+			cstmt.setString(1,id);
+			cstmt.registerOutParameter(2,Types.VARCHAR);
+			cstmt.registerOutParameter(3,Types.VARCHAR);
+			cstmt.registerOutParameter(4,Types.INTEGER);
+			cstmt.registerOutParameter(5,Types.FLOAT);
+			cstmt.execute();
+			System.out.println("Employee Data:");
+			System.out.println("Employee id :"+ id);
+			System.out.println("Employee Name:"+ cstmt.getString(2));
+			System.out.println("Employee Designation:"+ cstmt.getString(3));
+			System.out.println("Employee Basic salary:"+ cstmt.getInt(4));
+			System.out.println("Employee Total salary:"+ cstmt.getFloat(5));
+		}
+		catch(Exception e) {
+		  
+			//e.printStackTrace();
+			System.out.println("Record is not found");
+		}
+	}
+	void meth3() {
+		Connection con=connect();
+		try {
+			CallableStatement cstmt=con.prepareCall("{? = call RetrieveTotalSal(?)}");
+			System.out.println("Enter Employee ID:");
+			String id=sc.nextLine();
+			cstmt.setString(2, id);
+			cstmt.registerOutParameter(1,Types.INTEGER);
+			cstmt.execute();
+			System.out.println("Employee id:"+ id);
+			System.out.println("Employee Total Salary:"+cstmt.getInt(1));
+		}
+		catch(Exception e) {
+			System.out.println("Record is not found");
+		}
+	}
 	public static void main(String[] args) {
 		Jdbc_APP10 obj=new Jdbc_APP10();
-		obj.meth1();
+		//obj.meth1();
+		//obj.meth2();
+		obj.meth3();
 	}
 }
